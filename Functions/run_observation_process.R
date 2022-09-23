d6bad5f2d6bfddaef40b8184ac1b15e680b8b646
emilygsimmonds <emilygsimmonds@gmail.com>
2022-09-23 14:56:16 +0100

    Added code to scale number of offspring by the lifetime recapture probability

--- Functions/run_observation_process.R
+++ Functions/run_observation_process.R
@@ -13,11 +13,11 @@ library(tidyverse)
 # ID (factor), Year (factor), Surv (0/1), Offspring (num), 
 # Age (num), Trait (num)
 #
-# - p = recapture probability
+# - p_adult = adult recapture probability
 # 
 # - fecundity error = TRUE/FALSE, is there error in fecundity data?
 #
-# - juv_recapture = TRUE/FALSE, is capture of juveniles 100% or not
+# - p_juvenile = probability of capture for juveniles
 
 
 ## OUTPUT = dataframe of observed data
@@ -26,21 +26,46 @@ library(tidyverse)
 
 
 run_observation_process <- function(state_data, 
-                                    p, 
+                                    p_adult, 
                                     fecundity_error = TRUE,
-                                    juv_recapture = TRUE){
+                                    p_juvenile,
+                                    phi_adult,
+                                    phi_juvenile,
+                                    fecundity){
+  
+## Calculate probability of EVER being recaptured alive
+  
+# captured alive year 1
+year1 <- dbinom(1, 1, p_juvenile) # probability of initial capture at 1 year
+
+# not captured year 1 but captured year 2
+year2 <- prod(dbinom(0, 1, p_juvenile),
+              dbinom(1, 1, (p_adult*phi_juvenile))) 
+# probability of surviving to year 2 and capture
+
+# not captured year 1 or 2 but captured year 3-5
+year3 <- prod(dbinom(0, 1, p_juvenile),
+              dbinom(0, 1, (p_adult*phi_juvenile)),
+              sum(dbinom(1:3, 3, (p_adult*phi_adult)))) # probability of surviving beyond year 2 and capture
+
+total_prob <- sum(year1, year2, year3)   
+
+# combine the recapture probabilities into a vector
+recapture <- c(p_juvenile, rep(p_adult, max(state_data$Age)))
+  
 # fill in recapture probabilities
 # as survival is "did they survive until next year?" it is sort of lagged
 # recapture wants to refer to the focal year so cannot use survival to define it
 # instead will just same from rbinom
-if(juv_recapture == TRUE){
-  observed_data <- state_data %>% 
-    mutate(Recapture = rbinom(length(state_data$Age), 1, p)) %>%
-    mutate(Recapture = case_when(Age == 1 ~ 1,
-                                 TRUE ~ as.numeric(Recapture)))}
-if(juv_recapture == FALSE){
-  observed_data <- state_data %>% 
-    mutate(Recapture = rbinom(length(state_data$Age), 1, p))}
+
+# index the probability based on age
+observed_data <- state_data %>% 
+    mutate(Recapture = rbinom(length(state_data$Age), 1, 
+                              recapture[state_data$Age]))
+
+# scale fecundity by chance of ever being observed
+observed_data$Offspring <- rpois(length(observed_data$Offspring),
+                                 (fecundity*total_prob))
 
 # add observation error to fecundity - poisson error (count error)
 if(fecundity_error == TRUE){
